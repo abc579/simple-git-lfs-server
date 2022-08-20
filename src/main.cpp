d3619db1c4f8a7977aabcfbddd7b6aafb3b853a4
lev <flevn@protonmail.com>
2022-08-20 17:09:04 +0200

    Minor fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,20 @@
 #include <memory>
 
 #include "httplib.h"
+#include "get_storage_option.h"
 #include "httplib/httplib_lfs_server.h"
 #include "server/config.h"
 #include "server/ilfs_server.h"
-#include "get_storage_option.h"
 
 int main() {
-  using namespace server;
-  using namespace storage;
   try {
-    config cfg;
-    auto storage_opt = get_storage_option(cfg.storage_option(), cfg.file_directory());
-    auto sv = std::make_unique<lfs::httplib_lfs_server>(cfg, *storage_opt);
+    server::config cfg;
+    std::unique_ptr<storage::istorage> storage_opt =
+        get_storage_option(cfg.storage_option(), cfg.file_directory());
+    std::unique_ptr<lfs::ilfs_server> sv =
+        std::make_unique<lfs::httplib_lfs_server>(cfg, *storage_opt);
     sv->listen();
-  } catch (const config_error& err) {
+  } catch (const server::config_error& err) {
     std::cerr << "Config_error: " << err.what() << std::endl;
     return EXIT_FAILURE;
   } catch (const std::exception& err) {
